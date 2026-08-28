6495a6e286db72e01fdd2b5ec9f13b60cfeadab0
beyondvisions <rouarezgui8@gmail.com>
2026-08-28 17:52:18 +0100

    feat: add BooksController skeleton with POST /books

---
+++ src/ReadingList/Controllers/BooksController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReadingList.Controllers;
+
+[ApiController]
+[Route("books")]
+public class BooksController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Create()
+    {
+        return Ok("BooksController is alive");
+    }
+}
