357cb6a4a88729d502f7bbd739e20a1aed45feae
beyondvisions <rouarezgui8@gmail.com>
2026-08-28 22:53:17 +0100

    feat: wire BooksController to BookService with validation

--- src/ReadingList/Controllers/BooksController.cs
+++ src/ReadingList/Controllers/BooksController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using ReadingList.Contracts;
+using ReadingList.Services;
 
 namespace ReadingList.Controllers;
 
@@ -6,9 +9,30 @@ namespace ReadingList.Controllers;
 [Route("books")]
 public class BooksController : ControllerBase
 {
-    [HttpPost]
-    public IActionResult Create()
+    private readonly IBookService _bookService;
+    private readonly IValidator<BookRequest> _validator;
+
+    public BooksController(IBookService bookService, IValidator<BookRequest> validator)
     {
-        return Ok("BooksController is alive");
+        _bookService = bookService;
+        _validator = validator;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create(BookRequest request, CancellationToken ct)
+    {
+        var result = await _validator.ValidateAsync(request, ct);
+        if (!result.IsValid)
+        {
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+
+            return ValidationProblem(ModelState);
+        }
+
+        var book = await _bookService.CreateAsync(request, ct);
+        return CreatedAtAction(nameof(Create), new { id = book.Id }, book);
     }
 }
