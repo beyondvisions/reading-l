341013d811530c316960ead8a10e934557e5b151
beyondvisions <rouarezgui8@gmail.com>
2026-08-28 22:55:21 +0100

    feat: add contracts, enums, service and validator for create book

---
+++ src/ReadingList/Enums/BookStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace ReadingList.Enums;
+
+public enum BookStatus
+{
+    ToRead = 0,
+    Reading = 1,
+    Finished = 2,
+    Abandoned = 3
+}
---
+++ src/ReadingList/Services/IBookService.cs
@@ -0,0 +1,8 @@
+﻿using ReadingList.Contracts;
+
+namespace ReadingList.Services;
+
+public interface IBookService
+{
+    Task<BookResponse> CreateAsync(BookRequest request, CancellationToken ct = default);
+}
