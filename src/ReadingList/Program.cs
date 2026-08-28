816dd8b8a862840f3ffaf6010cfa8c17e309121c
beyondvisions <rouarezgui8@gmail.com>
2026-08-28 20:14:36 +0100

    feat: add BookRequest validator

--- src/ReadingList/Program.cs
+++ src/ReadingList/Program.cs
@@ -1,8 +1,10 @@
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();   
 builder.Services.AddOpenApi();
-
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
