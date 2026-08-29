2b49eb4f80f2968c940a32efe84f932196a1f2b6
beyondvisions <rouarezgui8@gmail.com>
2026-08-29 10:47:04 +0100

    Move validation to global ValidationFilter, configure Serilog file sink

--- src/ReadingList/Controllers/BooksController.cs
+++ src/ReadingList/Controllers/BooksController.cs
@@ -1,5 +1,4 @@
-﻿using FluentValidation;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ReadingList.Contracts;
 using ReadingList.Services;
 
@@ -10,12 +9,10 @@ namespace ReadingList.Controllers;
 public class BooksController : ControllerBase
 {
     private readonly IBookService _bookService;
-    private readonly IValidator<BookRequest> _validator;
 
-    public BooksController(IBookService bookService, IValidator<BookRequest> validator)
+    public BooksController(IBookService bookService)
     {
         _bookService = bookService;
-        _validator = validator;
     }
 
     [HttpPost]
@@ -23,15 +20,6 @@ public class BooksController : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Create(BookRequest request, CancellationToken ct)
     {
-        var result = await _validator.ValidateAsync(request, ct);
-        if (!result.IsValid)
-        {
-            foreach (var error in result.Errors)
-                ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
-
-            return ValidationProblem(ModelState);
-        }
-
         var book = await _bookService.CreateAsync(request, ct);
         return CreatedAtAction(nameof(Create), new { id = book.Id }, book);
     }
--- src/ReadingList/Program.cs
+++ src/ReadingList/Program.cs
@@ -1,12 +1,22 @@
 using FluentValidation;
-using ReadingList.Services;  
+using ReadingList.Filters;
+using ReadingList.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();   
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ValidationFilter>();
+});
+
 builder.Services.AddOpenApi();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services.AddScoped<IBookService, BookService>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -14,7 +24,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
-app.MapControllers();                
+app.MapControllers();
 
 app.Run();
