fa2e57e74920d528ab36d85a11a7ed89aa08e537
beyondvisions <rouarezgui8@gmail.com>
2026-08-30 14:35:35 +0100

    refactor: extract service registrations into extension methods

---
+++ src/ReadingList/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ReadingList.Data;
+using ReadingList.Filters;
+using ReadingList.Repositories;
+using ReadingList.Services;
+
+namespace ReadingList.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ReadingListDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IBookRepository, BookRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IBookService, BookService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddControllers(options =>
+        {
+            options.Filters.Add<ValidationFilter>();
+        });
+
+        services.AddOpenApi();
+        services.AddValidatorsFromAssemblyContaining<Program>();
+
+        return services;
+    }
+}
--- src/ReadingList/Program.cs
+++ src/ReadingList/Program.cs
@@ -1,9 +1,4 @@
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using ReadingList.Data;
-using ReadingList.Filters;
-using ReadingList.Repositories;
-using ReadingList.Services;
+using ReadingList.Extensions;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,17 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((context, config) =>
     config.ReadFrom.Configuration(context.Configuration));
 
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<ValidationFilter>();
-});
-
-builder.Services.AddOpenApi();
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-builder.Services.AddScoped<IBookService, BookService>();
-builder.Services.AddScoped<IBookRepository, BookRepository>(); 
-builder.Services.AddDbContext<ReadingListDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddApiServices();
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
